ad5a48945c52aeff6fa9fe011fa992c6ba38569b
DenisKoriavets <d.korjavets@ukr.net>
2026-03-02 10:16:32 +0200

    Add input and output functions for console and file handling

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def input_from_console() -> str:
+    pass
+
+def read_from_file(file_path: str) -> str:
+    pass
+
+def read_from_file_pandas(file_path: str) -> str:
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def output_to_console(text: str) -> None:
+    pass
+
+def output_to_file(text: str) -> None:
+    pass
